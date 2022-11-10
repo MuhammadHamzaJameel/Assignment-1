3bab697ae7c8f2e868bcbdd9b5117936869836da
Muhammad Hamza <MuhammadHamza@corp.contoursoftware.net>
2022-11-10 18:04:40 +0500

    Updating index.js

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
+var selectedRow = null;
 window.onload = function() {
 document.getElementById('btnAddData').addEventListener("click", insertData);
 }
 
+function onSubmit(e){
+  e.preventDefault();
+}
+
 function insertData(event) {
   event.preventDefault();
-  const form = document.getElementById("form");
-  const firstname = document.getElementById("firstname");
-  const lastname = document.getElementById("lastname");
-  const email = document.getElementById("email");
-  const password = document.getElementById("password");
-  const confirmPassword = document.getElementById("password2");
-  const age = document.getElementById("age");
+  const firstname = document.getElementById("firstname").value;
+  const lastname = document.getElementById("lastname").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const age = document.getElementById("age").value;
+  const display = document.getElementById("table");
 
-  const row = .insertRow((0));
+  const row = display.insertRow(0);
 
-  cell1 = row.insertcell(0);
-  cell2 = row.insertcell(1);
-  cell3 = row.insertcell(2);
-  cell4 = row.insertcell(3);
-  cell5 = row.insertcell(4);
-  cell6 = row.insertcell(5);
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 =row.insertCell(2);
+  const cell4 = row.insertCell(3);
+  const cell5 = row.insertCell(4);
+  const cell6 = row.insertCell(5);
 
   cell1.innerHTML = firstname;
   cell2.innerHTML = lastname;
   cell3.innerHTML = email;
   cell4.innerHTML = password;
-  cell5.innerHTML= confirmPassword;
-  cell6.innerHTML= age;
-
+  cell5.innerHTML= age;
+  cell6.innerHTML = "<button type='button' id='btnEditData' onclick='editData(this)'>Edit Data</button>"
+  document.getElementById("form").reset();
 }
+
+function editData(td){
+  selectedRow = td.parentElement.parentElement;
+  document.getElementById("firstname").value = selectedRow.cells[0].innerHTML;
+  document.getElementById("lastname").value = selectedRow.cells[1].innerHTML;
+  document.getElementById("email").value = selectedRow.cells[2].innerHTML;
+  document.getElementById("password").value = selectedRow.cells[3].innerHTML;
+  document.getElementById("age").value = selectedRow.cells[4].innerHTML;
+}
+function updateData(formData){
+  selectedRow.cells[0].innerHTML = formData.firstname;
+  selectedRow.cells[1].innerHTML = formData.lastname;
+  selectedRow.cells[2].innerHTML = formData.email;
+  selectedRow.cells[3].innerHTML = formData.password;
+  selectedRow.cells[4].innerHTML = formData.age;
+}
+
